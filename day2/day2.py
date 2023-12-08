1f076c1f5c4ea993057b4e23422b97f06b0e8d4f
deraan2099 <deraan.ranferte@gmail.com>
2023-12-08 16:39:39 +0100

    day2 part 1

---
+++ day2/day2.py
@@ -0,0 +1,23 @@
+import numpy as np
+#s = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+valid_games = []
+with open("input.txt") as infile:
+    for line in infile:
+        print(line)
+        game_id, sets = line.split(':')[0].split(' ')[-1], line.split(':')[-1].split(';')
+        max_matrix = np.array([12,13,14]*len(sets)).reshape(len(sets),3)
+        games_vec = np.zeros(3*len(sets))
+        for i,game in enumerate(sets):
+            k = i*3
+            for dice in game.split(','):
+                if 'red' in dice:
+                    games_vec[k] = int(dice.split('red')[0])
+                elif 'green' in dice:
+                    games_vec[k+1] = int(dice.split('green')[0])
+                elif 'blue' in dice:
+                    games_vec[k+2] = int(dice.split('blue')[0])
+
+        check = games_vec.reshape(len(sets),3) > max_matrix
+        if check.sum() == 0:
+            valid_games += [game_id]
+print(sum([int(x) for x in valid_games]))
